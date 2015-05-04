42724989888205ff4b596dec7ce8ffba6a4a1e50
Mark & Dan Benjamin <pair+Makdash49.dan@devbootcamp.com>
2015-05-04 12:13:45 -0700

    Release 1 Solution

--- todo_list.js
+++ todo_list.js
@@ -1,15 +1,39 @@
 var createTodoList = function() {
+  // console.log(this)
   var todoList = {};
 
   // your code here
+  todoList.collection = []
+  todoList.add = function(item){
+    this.collection.push(item);
+  };
+
+  todoList.list = function(){
+      console.log("Grocery List: " + this.collection)
+  };
+
+  todoList.indexOf = function(item){
+    for (var i = 0; i <= this.collection.length; i++){
+      if (this.collection[i] === item)
+      {
+        console.log("This is the index: " + i);
+        break;
+      } else if (i === this.collection.length){
+        console.log("Item not found!")
+      }
+    }
+    console.log()
+  };
+
+  todoList.remove = function(index){
+    this.collection.splice(index, 1)
+  };
+
+  todoList.get = function(){};
+  todoList.complete = function(){};
 
   return todoList;
 };
-
-
-
-
-
 
 // Driver code
 
@@ -31,20 +55,20 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
