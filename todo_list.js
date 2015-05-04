b8cf9035ee9b18f47e77fb2f40c26e09f676fdbd
Caitlyn & What did the fox say? <pair+heycait.nguyennam9696@devbootcamp.com>
2015-05-04 11:29:18 -0700

    finish release 1

--- todo_list.js
+++ todo_list.js
@@ -1,12 +1,24 @@
 var createTodoList = function() {
-  var todoList = {};
+  var todoList = {
+    tasks: [],
+    add: function(item) {
+      this.tasks.push(item);
+    },
+    list: function(item) {
+      for (var i = 0; i < this.tasks.length; i++) {
+        console.log(this.tasks[i])
+      };
+    },
+    indexOf: function(item) {
+      this.tasks[item];
+    },
+    remove: function(item_index) {
+      this.tasks.splice(item_index,1);
+    },
+  };
 
-  // your code here
-
-  return todoList;
+return todoList;
 };
-
-
 
 
 
@@ -31,20 +43,21 @@ groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
+
